f11b6c83f5a4f4690b119e5dbeec924bc4648bb6
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2023-01-18 20:34:18 +0300

    Delete Scripts directory

--- Scripts/Input/SquadInput/SquadBrain.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SquadBrain : MonoBehaviour
-{
-    public abstract void Add(WarriorSystem I, bool IsCommander);
-    public abstract void Remove(WarriorSystem I, bool IsCommander);
-}
--- Scripts/Interfaces/SquadBrain.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface SquadBrain
-{
-    void Add(WarriorSystem I, bool IsCommander);
-    void Remove(WarriorSystem I, bool IsCommander);
-}
