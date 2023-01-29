1d11c974837a7e9c33a3dd45c0d7168af39d8302
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2023-01-29 13:19:32 +0300

    Delete Scripts directory

--- Scripts/Weapon/WeaponOnGround.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponOnGround : MonoBehaviour
-{
-    [SerializeField]
-    private WeaponInHand _weaponInHand;
-}
